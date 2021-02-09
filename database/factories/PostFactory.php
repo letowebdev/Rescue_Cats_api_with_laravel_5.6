3015e92c1011c2373ae4b58d49b391fbd11f36df
letowebdev <a.zache@outlook.de>
2021-02-09 23:08:42 +0100

    adding comments

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
     return [
+        'user_id' => function() {
+            return factory(User::class)->create()->id;
+        },
         'title' => $title = $faker->sentence,
         'slug' => str_slug($title),
         'body' => $faker->paragraph
--- database/migrations/2021_02_09_212852_create_comments_table.php
+++ database/migrations/2021_02_09_212852_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
-            $table->string('title');
-            $table->string('slug')->unique();
+            $table->integer('post_id');
             $table->text('body');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('post_id')->references('id')->on('posts');
         });
     }
 
@@ -32,6 +32,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 }

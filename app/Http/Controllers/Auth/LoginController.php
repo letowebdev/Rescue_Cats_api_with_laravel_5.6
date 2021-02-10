15aa5de0ea57c7012124639e4e6090f980abfe98
letowebdev <a.zache@outlook.de>
2021-02-10 17:49:40 +0100

    authenticating a user

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\Users\PrivateUserResource;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function action(LoginRequest $request)
+    {
+        if (!$token = Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'errors' => [
+                    'email' => ['Couldn\'t sign you in, please check your credentials and ty again']
+                ]
+                ], 422);
+        }
+
+        return (new PrivateUserResource($request->user()))
+                ->additional([
+                    'meta' => [
+                        'token' => $token
+                    ]
+                ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ Route::resource('posts/{post}/comments', 'Comments\CommentController');
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('register', 'Auth\RegisterController@action');
+    Route::post('login', 'Auth\LoginController@action');
 });

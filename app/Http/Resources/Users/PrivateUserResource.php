6e574cb11a3b5a3a9fd67517c6ae157eefaeb293
letowebdev <a.zache@outlook.de>
2021-02-11 11:41:46 +0100

    updating user resource

--- app/Http/Resources/Users/PrivateUserResource.php
+++ app/Http/Resources/Users/PrivateUserResource.php
@@ -18,7 +18,12 @@ class PrivateUserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'username' => $this->username,
-            'email' => $this->email
+            'email' => $this->email,
+            'about' => $this->about,
+            'tagline' => $this->tagline,
+            'formatted_address' => $this->formatted_address,
+            'location' => $this->location,
+            'available_to_hire' => $this->available_to_hire,
         ];
     }
 }

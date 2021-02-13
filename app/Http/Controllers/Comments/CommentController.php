1038ee38f53a4c172431975ca0fb22aeee8a43ff
letowebdev <a.zache@outlook.de>
2021-02-13 11:33:35 +0100

    adding the comment crud methods

--- app/Http/Controllers/Comments/CommentController.php
+++ app/Http/Controllers/Comments/CommentController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Comments;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Comments\CommentRequest;
+use App\Http\Resources\Comments\CommentResource;
 use App\Http\Resources\Posts\PostResource;
+use App\Models\Comment;
 use App\Models\Post;
-use App\Models\User;
 
 class CommentController extends Controller
 {
@@ -15,13 +16,31 @@ class CommentController extends Controller
         $this->middleware('auth:api');
     }
 
-    public function store(Post $post)
+    public function store(Post $post, CommentRequest $request)
     {
         $post->addComment(([
             'user_id' => auth()->user()->id,
-            'body' => request('body')
+            'body' => $request->body,
             ]));
 
         return new PostResource($post);
+    }
+
+    public function update(CommentRequest $request, Comment $comment)
+    {
+        $comment->update([
+            'body' => $request->body
+        ]);
+
+        return new CommentResource($comment);
+    }
+
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+
+        return response()->json([
+            "message" => "Comment deleted"
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,9 @@ Route::resource('posts', 'Posts\PostController');
 
 Route::get('users', 'Users\UserController@index');
 
-Route::post('posts/{post}/comments', 'Comments\CommentController@store');
+Route::post('posts/{post}/comments', 'Comments\CommentController@store')->name('addComment');
+Route::put('comments/{comment}', 'Comments\CommentController@update')->name('updateComment');
+Route::delete('comments/{comment}', 'Comments\CommentController@destroy')->name('deleteComment');
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('register', 'Auth\RegisterController@action');
